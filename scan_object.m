2a88bf6dc100ed6c6855a42c955498b2149de2d9
Daniel Tar <tardani95@gmail.com>
2018-05-03 18:10:45 +0200

    cycle variable changed to idr

--- scan_object.m
+++ scan_object.m
@@ -5,12 +5,12 @@ grayImage=zeros(480,640);
 kepek=zeros(rotations,480,640);
 figure;
 f=waitbar(0,'Making images');
-for idx=1:rotations
-    printf('%d/%d\n', idx, rotations);
+for idr=1:rotations
+    printf('%d/%d\n', idr, rotations);
     
     laser_line_detection;
     rotateTable(a,clk,10);
     %createPolarPointSet;
-    waitbar(idx/rotations,f,'Making images');
+    waitbar(idr/rotations,f,'Making images');
 end
 close(f)
