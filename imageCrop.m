0cc574c051c29a5763f2bccc93073bea22ebc42a
Roland Varga <roland.varga601@gmail.com>
2018-05-03 02:31:33 +0200

    imageCrop function added

---
+++ imageCrop.m
@@ -0,0 +1,13 @@
+function [rect,NullNull,nullnull]=imageCrop
+img=imread('Image21.png');
+[croppedimage, rect] = imcrop(img);
+nullnull(1)=cameraParams.ReprojectedPoints(1,1,21)-rect(1);
+nullnull(2)=cameraParams.ReprojectedPoints(1,2,21)-rect(2);
+
+NullNull=[cameraParams.ReprojectedPoints(24,1,21)-rect(1)...
+    cameraParams.ReprojectedPoints(24,2,21)-rect(2)];
+
+squareInPixels=(cameraParams.ReprojectedPoints(6,2,21)-...
+    cameraParams.ReprojectedPoints(1,2,21))/5;
+
+NullNull(2)=NullNull(2)+squareInPixels;
