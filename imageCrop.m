2fbbabce6ae5383bb65442d39035d35b790cf9ca
Roland Varga <roland.varga601@gmail.com>
2018-05-03 18:15:33 +0200

    Global zero added

--- imageCrop.m
+++ imageCrop.m
@@ -1,13 +1,17 @@
-function [rect,NullNull,nullnull]=imageCrop
+function [rect,rotationAxesOrigo]=imageCrop
+
 img=imread('calibration_images\Image21.png');
 [croppedimage, rect] = imcrop(img);
-nullnull(1)=cameraParams.ReprojectedPoints(1,1,21)-rect(1);
-nullnull(2)=cameraParams.ReprojectedPoints(1,2,21)-rect(2);
+%nullnull(1)=cameraParams.ReprojectedPoints(1,1,21)-rect(1);
+%nullnull(2)=cameraParams.ReprojectedPoints(1,2,21)-rect(2);
 
-NullNull=[cameraParams.ReprojectedPoints(24,1,21)-rect(1)...
-    cameraParams.ReprojectedPoints(24,2,21)-rect(2)];
+%A forgástengelyen lévõ origó, elõször y (lefelé mutató),
+%   utána x koordináta.
+rotationAxesOrigo=[cameraParams.ReprojectedPoints(24,2,21)-rect(2)...
+    cameraParams.ReprojectedPoints(24,1,21)-rect(1)];
 
 squareInPixels=(cameraParams.ReprojectedPoints(6,2,21)-...
     cameraParams.ReprojectedPoints(1,2,21))/5;
 
-NullNull(2)=NullNull(2)+squareInPixels;
+rotationAxesOrigo(1)=rotationAxesOrigo(1)+squareInPixels;
+
