48ec9b5866882a4cf238448d377ccb4373b1ec70
Roland Varga <roland.varga601@gmail.com>
2018-05-15 01:34:29 +0200

    new rotation height calculation - operates only with one filter

--- createPolarPointSet.m
+++ createPolarPointSet.m
@@ -1,42 +1,25 @@
-imgSize = size(laserMask1);
+function polarPointSet_mm=createPolarPointSet(tfLaserMask,polarPointSet_mm,idr,xdata,ydata)
+imgSize = size(tfLaserMask);
 imgHeight = imgSize(1);
 imgWidth  = imgSize(2);
 
 % the first coordinate is the radius in [mm]
 % the second coordinate is the height in [mm]
-polarPointSet1_mm = zeros([rotations imgHeight 2]);
+
+%polarPointSet2_mm = zeros([rotations imgHeight 2]);    !!!!!!
 for iy = 1:imgHeight
     pointCounter = 0;
-    pointLocation = zeros([1 1]);
+    pointLocation = 0;
     for ix = 1:imgWidth
-        if laserMask1(iy,ix) == 1
+        if tfLaserMask(iy,ix) ~= 0
             pointCounter  = pointCounter + 1;
-            pointLocation = pointLocation + [iy ix];
+            pointLocation = pointLocation + ix;
         end    
     end
     if pointCounter ~= 0
-       pointLocation = pointLocation / pointCounter;
-       positionVector = pointLocation - rotationAxisOrigo; % y,x
-       rotation_height = calcRotationAndHeight(verticalVector,horizontalVector,positionVector); 
-       polarPointSet1_mm(idr,iy,:) = rotation_height;
-    end       
-end
-
-
-polarPointSet2_mm = zeros([rotations imgHeight 2]);
-for iy = 1:imgHeight
-    pointCounter = 0;
-    pointLocation = zeros([1 1]);
-    for ix = 1:imgWidth
-        if laserMask2(iy,ix) == 1
-            pointCounter  = pointCounter + 1;
-            pointLocation = pointLocation + [iy ix];
-        end    
-    end
-    if pointCounter ~= 0
-       pointLocation = pointLocation / pointCounter;
-       positionVector = pointLocation - rotationAxisOrigo; % y,x
-       rotation_height = calcRotationAndHeight(verticalVector,horizontalVector,positionVector); 
-       polarPointSet2_mm(idr,iy,:) = rotation_height;
+        pointLocation = pointLocation / pointCounter;
+        positionVector = [pointLocation,iy];
+        rotation_height = [positionVector(1)+xdata(1) positionVector(2)-ydata(2)];
+        polarPointSet_mm(idr,iy,:) = rotation_height;
     end       
 end
