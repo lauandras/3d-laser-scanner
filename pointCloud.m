7001d818712af734c4e659d4e231f0d24a27a6db
Roland Varga <roland.varga601@gmail.com>
2018-05-15 01:43:26 +0200

    creates a xyz pointcloud from the polarPointSet

---
+++ pointCloud.m
@@ -0,0 +1,15 @@
+function xyz=pointCloud(rotations,polarPointSet_mm,tfHeight)
+xyz=zeros([tfHeight*rotations,3]);
+for i=1:rotations
+    angle=2*pi/(rotations-1)*(i-1);
+    for j=1:tfHeight
+        xyz((i-1)*tfHeight+j,1)=polarPointSet_mm(i,j,1)*cos(angle);
+        xyz((i-1)*tfHeight+j,2)=polarPointSet_mm(i,j,1)*sin(angle);
+        if (polarPointSet_mm(i,j,2)>0)
+%         if (0) 
+            xyz((i-1)*tfHeight+j,3)=0;
+        else
+            xyz((i-1)*tfHeight+j,3)=-polarPointSet_mm(i,j,2);
+        end
+    end
+end
