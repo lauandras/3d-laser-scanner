fb2d363e336d6b751375bb7635f37fd8d986f538
Roland Varga <roland.varga601@gmail.com>
2018-05-17 00:06:05 +0200

    Cameracalibration adatait sajatra kell meg cserelni

--- matlab_files/imageTform.m
+++ matlab_files/imageTform.m
@@ -1,11 +1,11 @@
-function [tfHeight,tfWidth,tform,xdata,ydata]=imageTform(imgCropped,rect,paralellImageNumber,cameraParams)
+function [tfHeight,tfWidth,tform,xdata,ydata]=imageTform(imgCropped,rect,paralellImageNumber,tfPoints)
 
-v4=cameraParams.ReprojectedPoints(1,:,paralellImageNumber)-[rect(1) rect(2)];
-v1=cameraParams.ReprojectedPoints(6,:,paralellImageNumber)-[rect(1) rect(2)];
-v3=cameraParams.ReprojectedPoints(49,:,paralellImageNumber)-[rect(1) rect(2)];
-v2=cameraParams.ReprojectedPoints(54,:,paralellImageNumber)-[rect(1) rect(2)];
+v4=tfPoints(4,:)-[rect(1) rect(2)];
+v1=tfPoints(1,:)-[rect(1) rect(2)];
+v3=tfPoints(3,:)-[rect(1) rect(2)];
+v2=tfPoints(2,:)-[rect(1) rect(2)];
 
-worldCoordinates=14.55*[-4 6; -4 1; 4 6; 4 1];
+worldCoordinates=14.55*[-4 -6; -4 -1; 4 -6; 4 -1];
 
 tform = maketform('projective',[v4; v1; v3; v2],worldCoordinates);
 [newImage,xdata,ydata]=imtransform(imgCropped,tform);
